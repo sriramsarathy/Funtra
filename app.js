728730ae23a54925f4f832d967343865f846967e
Sriram <psriram26@gmail.com>
2018-09-08 09:04:46 +0530

    adding server listening

--- app.js
+++ app.js
@@ -63,3 +63,15 @@ app.get('/person', function (req, res) {
     res.send(JSON.stringify(documents));
     });
 });
+
+if (module === require.main) {
+  // [START server]
+  // Start the server
+  const server = app.listen(process.env.PORT || 8080, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`);
+  });
+  // [END server]
+}
+
+module.exports = app;
